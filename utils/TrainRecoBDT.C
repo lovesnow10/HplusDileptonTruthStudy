0cc211219fa2355ba7f11c8d09372c7c9ca5fbdc
xuyang <yangx_chn@hotmail.com>
2016-11-24 05:37:16 +0800

    Add BDT training

---
+++ utils/TrainRecoBDT.C
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TMVA/Factory.h"
+#include "TString.h"
+
+#include "Utilities.h"
+#include "Tools.h"
+
+int main(int argc, char const *argv[]) {
+  //open files and get trees
+  TFile *inFile = OpenFile(argv[1]);
+  TFile *outFile = CreateNewFile(argv[2]);
+
+  TTree *mSigTree = GetTTree("signal", inFile);
+  TTree *mBkgTree = GetTTree("background", inFile);
+
+  //definde options
+  TString classifierName = "RecoBDT_Dilepton";
+  TString trainingOptions;
+  TString classifierOptions;
+
+  trainingOptions = ":NormMode=NumEvents";
+  classifierOptions += ":!H";
+  classifierOptions += ":NTrees=500";
+  classifierOptions += ":BoostType=Grad";
+  classifierOptions += ":Shrinkage=0.1";
+  classifierOptions += ":UseBaggedBoost";
+  classifierOptions += ":GradBaggingFraction=0.5";
+  classifierOptions += ":nCuts=20";
+  classifierOptions += "MaxDepth=3";
+
+  std::vector<TString> mVariables;
+  mVariables.push_back("PseWp_Mass");
+  mVariables.push_back("PseWm_Mass");
+  mVariables.push_back("PseTop_Mass");
+  mVariables.push_back("PseTbar_Mass");
+  mVariables.push_back("PseHplus_Mass");
+  mVariables.push_back("dR_Lp_Lm");
+  mVariables.push_back("dR_B1_B2");
+  mVariables.push_back("dR_B1_B3");
+  mVariables.push_back("dR_B2_B3");
+  mVariables.push_back("Pse_dR_Wp_Wm");
+  mVariables.push_back("Pse_dR_ttbar");
+  mVariables.push_back("Pse_dR_Hp_tbar");
+
+  TCut cutSigTrain = "UsedForTrain == 1";
+  TCut cutSigTest = "UsedForTrain == 0";
+  TCut cutBkgTrain = "UsedForTrain == 1";
+  TCut cutBkgTest = "UsedForTrain == 0";
+
+  //Setup TMVA Factory and training BDT
+  TMVA::Factory factory("RecoBDT_Dilepton", outFile);
+  factory.SetInputVariables(&mVariables);
+  factory.AddTree(mSigTree, "Signal", 1.0, cutSigTrain, "train");
+  factory.AddTree(mSigTree, "Signal", 1.0, cutSigTest, "test");
+  factory.AddTree(mBkgTree, "Background", 1.0, cutBkgTrain, "train");
+  factory.AddTree(mBkgTree, "Background", 1.0, cutBkgTest, "test");
+
+  factory.PrepareTrainingAndTestTree("", trainingOptions);
+  factory.BookMethod(TMVA::Types::kBDT, classifierName, classifierOptions);
+  factory.TrainAllMethods();
+  factory.TestAllMethods();
+  factory.EvaluateAllMethods();
+
+  outFile->Close();
+  return 0;
+}
