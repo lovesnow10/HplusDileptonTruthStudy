85a9acfffbd7cf9f69e7def1a7e19186f8ef3b89
Xuan Yang <yangx_chn@hotmail.com>
2017-03-17 11:47:53 +0100

    fixed Plot script

--- python/PlotRecoBDTTest.py
+++ python/PlotRecoBDTTest.py
@@ -10,7 +10,7 @@ def GetBDTScoreHist(mFile, mHistName):
 
 
 def DrawPlots(mSigHist, mBkgHist):
-    canvas = rt.TCanvas(800, 600)
+    canvas = rt.TCanvas('c1', '', 800, 600)
     GhostHist = mSigHist.Clone('Ghost')
     for iBin in xrange(1, GhostHist.GetNbinsX() + 1):
         GhostHist.SetBinContent(iBin, 0.0)
@@ -31,13 +31,13 @@ def DrawPlots(mSigHist, mBkgHist):
     mSigHist.SetLineWidth(2)
     mBkgHist.SetLineWidth(2)
 
-    mSigHist.SetLineColor(rt:: kRed)
-    mBkgHist.SetLineColor(rt:: kBlue)
+    mSigHist.SetLineColor(2)
+    mBkgHist.SetLineColor(3)
 
     legend = rt.TLegend(0.75, 0.75, 0.95, 0.95)
 
     legend.AddEntry(mSigHist, mSigHist.GetName())
-    legend.AddEntry(mBkgHist, mBkgHist.GetName())
+    legend.AddEntry(mBkgHist, 't#bar{t}')
 
     mSigHist.Draw('same hist')
     mBkgHist.Draw('same hist')
@@ -62,11 +62,17 @@ def main(mSigFilePath, mBkgFilePath):
     mMassPoint = mSigFilePath.split('.')[-2]
     mMassPoint = mMassPoint.split('/')[-1]
 
-    mSigHist = GetBDTScoreHist(mSigFile, 'RecoBDT_Dilepton_' + mMassPoint + '_'mMassPoint)
+    mSigHist = GetBDTScoreHist(mSigFile, 'RecoBDT_Dilepton_' + mMassPoint + '_' + mMassPoint)
     mBkgHist = GetBDTScoreHist(
         mBkgFile, 'RecoBDT_Dilepton_' + mMassPoint + '_ttbarPP8')
 
     mSigHist.SetName(mMassPoint)
-    mBkgHist.SetName('t\bar{t}')
+    mBkgHist.SetName('ttbar')
+
+    mSigHist.Scale(1.0 / mSigHist.GetSumOfWeights())
+    mBkgHist.Scale(1.0 / mBkgHist.GetSumOfWeights())
 
     DrawPlots(mSigHist, mBkgHist)
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
