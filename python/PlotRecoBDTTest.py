458ced867e2c523a7fa3226c396ba4f0a88e2224
xuyang <yangx_chn@hotmail.com>
2017-03-17 10:56:33 +0100

    add python plot script

---
+++ python/PlotRecoBDTTest.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import ROOT as rt
+import sys
+import os
+
+
+def GetBDTScoreHist(mFile, mHistName):
+    return mFile.Get(mHistName)
+
+
+def DrawPlots(mSigHist, mBkgHist):
+    canvas = rt.TCanvas(800, 600)
+    GhostHist = mSigHist.Clone('Ghost')
+    for iBin in xrange(1, GhostHist.GetNbinsX() + 1):
+        GhostHist.SetBinContent(iBin, 0.0)
+
+    GhostHist.SetStats(0)
+    GhostHist.SetTitle('')
+    yMaximun = max(mSigHist.GetMaximum(), mBkgHist.GetMaximum())
+
+    GhostHist.SetMaximum(yMaximun * 1.7)
+    GhostHist.SetMinimum(0.0)
+
+    GhostHist.Draw()
+
+    GhostHist.GetXaxis().SetTitle('Max RecoBDT Score')
+    GhostHist.GetYaxis().SetTitle('1 / Events')
+
+    # Set hist styles
+    mSigHist.SetLineWidth(2)
+    mBkgHist.SetLineWidth(2)
+
+    mSigHist.SetLineColor(rt:: kRed)
+    mBkgHist.SetLineColor(rt:: kBlue)
+
+    legend = rt.TLegend(0.75, 0.75, 0.95, 0.95)
+
+    legend.AddEntry(mSigHist, mSigHist.GetName())
+    legend.AddEntry(mBkgHist, mBkgHist.GetName())
+
+    mSigHist.Draw('same hist')
+    mBkgHist.Draw('same hist')
+    legend.Draw()
+
+    GhostHist.SetTitle(mSigHist.GetName())
+
+    canvas.Print(mSigHist.GetName() + '.eps')
+
+
+def main(mSigFilePath, mBkgFilePath):
+    rt.gROOT.SetBatch(True)
+    if not os.path.isfile(mSigFilePath):
+        print 'Wrong Signal File Path!'
+        sys.exit(-1)
+    if not os.path.isfile(mBkgFilePath):
+        print 'Wrong Background File Path'
+        sys.exit(-1)
+    mSigFile = rt.TFile.Open(mSigFilePath)
+    mBkgFile = rt.TFile.Open(mBkgFilePath)
+
+    mMassPoint = mSigFilePath.split('.')[-2]
+    mMassPoint = mMassPoint.split('/')[-1]
+
+    mSigHist = GetBDTScoreHist(mSigFile, 'RecoBDT_Dilepton_' + mMassPoint + '_'mMassPoint)
+    mBkgHist = GetBDTScoreHist(
+        mBkgFile, 'RecoBDT_Dilepton_' + mMassPoint + '_ttbarPP8')
+
+    mSigHist.SetName(mMassPoint)
+    mBkgHist.SetName('t\bar{t}')
+
+    DrawPlots(mSigHist, mBkgHist)
